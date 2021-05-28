f97e9d7a7cf5bfe0da42421db46ed2e91df7128d
Code Sandbox <hello@codesandbox.io>
2021-05-28 13:55:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cfcb1268bed6f6753f877889e62b81b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235ba2e773e0a1e33cf9f75a01fb9349",
+    "url": "https://mbrucedogs.github.io/csb-13zg7/index.html"
+  },
+  {
+    "revision": "bee46e55a8a4c15c61ff",
+    "url": "https://mbrucedogs.github.io/csb-13zg7/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "d78519df44ec2c64bd4e",
+    "url": "https://mbrucedogs.github.io/csb-13zg7/static/js/2.ec2d356b.chunk.js"
+  },
+  {
+    "revision": "bee46e55a8a4c15c61ff",
+    "url": "https://mbrucedogs.github.io/csb-13zg7/static/js/main.518d8777.chunk.js"
+  },
+  {
+    "revision": "40b609354148b0b6f133",
+    "url": "https://mbrucedogs.github.io/csb-13zg7/static/js/runtime~main.3f32f953.js"
+  }
+]);
